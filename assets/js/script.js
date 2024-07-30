a9e4dca1b21a65153bae126bd65689afb6c3b758
marysousa2 <maryestudo2@gmail.com>
2024-07-30 15:31:46 -0300

    novo

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,25 @@ window.addEventListener("scroll", function () {
   }
 
 });
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.getElementById('modal');
+  const btn = document.getElementById('more-destinos-btn');
+  const span = document.getElementsByClassName('close')[0];
+
+  btn.onclick = function() {
+      modal.style.display = 'block';
+  }
+
+  span.onclick = function() {
+      modal.style.display = 'none';
+  }
+
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = 'none';
+      }
+  }
+});
